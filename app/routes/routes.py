881fc2c7e1d9a5cd099bd200b7ecae0a3782c347
Cdvx <cdvx@grizzly.com>
2018-08-21 21:27:51 +0300

    Add get question endpoint [finishes #159936149]

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -8,3 +8,14 @@ from app.models import Answer, Question, answersList, questionsList
 def get_questions():
     return jsonify({'questions': questionsList})
 
+@app.route('/api/v1/questions/<int:questionId>', methods=['GET'])
+def get_question(questionId):
+    for question in questionsList:
+        if question['questionId'] == questionId:
+            temp = {
+                'questionId': question['questionId'],
+                'topic': question['topic'],
+                'body': question['body']
+            }
+            return jsonify(temp)
+    return Response(json.dumps(['Question not Found']), status=404, mimetype='application/json')
