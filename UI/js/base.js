867f439176477e0d34931cc166acd9418e5b1879
Cdvx <cdvx@grizzly.com>
2018-09-18 00:53:20 +0300

    Import fetch, make simple GET [finishes #160571337]

--- UI/js/base.js
+++ UI/js/base.js
@@ -1,2 +1,9 @@
 /* Resgister new user */
-let signUp = (e)
+// let signUp = (e)
+var fetch = require("node-fetch")
+
+
+fetch('http://localhost:5000/api/v1/questions/1')
+.then(res => res.json())
+// .then(json => console.log(json));
+.then(json => console.log(JSON.stringify(json)))
