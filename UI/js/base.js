b72c987af4f49e9686fcf10e180d4f91e3ab5a0a
Cdvx <cdvx@grizzly.com>
2018-09-19 23:16:47 +0300

    Connect get questions and and get answers using fetch

--- UI/js/base.js
+++ UI/js/base.js
@@ -18,76 +18,118 @@ function append(parent, element){
     return parent.appendChild(element)
 }
 
-// fetch("http://127.0.0.1:5000/api/v1/questions")
-// .then(res=> res.json())
-// // .then(res => res.json())
-// .then(json => {
-//     console.log(json)
-// })
-// .catch(error => console.error(error));
+function fetchAnswers(questionId){
+    console.log(document.title == "StackOverflow-lite-question");
+    if (document.title == "StackOverflow-lite-question"){
+        fetch(`http://127.0.0.1:5000/api/v1/questions/${questionId}/answers`, {
+            mode: "cors",
+        })
+        .then(res => res.json())
+        .then(json =>{
+            console.log(json);
+            if ("message" in json){
+                console.log(json.message);
+                let Qtns2 = document.getElementById('Qtns2');
+                    Qtn = createNode('p');
+    
+                Qtn.innerHTML = `${json.message}`
+                append(Qtns2, Qtn)
+            }
+            if ("answers" in json){
+                for (let answer of  json.answers){
+                    let li = createNode('li'),
+                        span = createNode('span'),
+                        span2 = createNode('span'),
+                        strong = createNode('strong');
+                        
+                        Qtns2 = document.getElementById('Qtns2');
+                        Qtn = createNode('p');
+                    
 
+                    if (answer.prefered == true){
+                        strong.classList.add("prefered");
+                        span.innerHTML = ` <strong>Author:</strong> ${answer.author}`;
+                        span2.innerHTML = `${answer.body} <br>`;
+                        strong.innerHTML = "Prefered answer";
+                        append(li, span2);
+                        append(li, span);
+                        append(li, strong);
+                        append(Qtn, li);
+                        // append(a, Qtn);
+                        append(Qtns, Qtn);
+                    }
+                    if (answer.prefered == false){
+                        span.innerHTML = ` <strong>Author:</strong> ${answer.author}`;
+                        span2.innerHTML = `${answer.body} <br>`;
+                        append(li, span2);
+                        append(li, span);
+                        append(Qtn, li);
+                        // append(a, Qtn);
+                        append(Qtns, Qtn);
+                    }
+                }
+            }
+        });
+    }
+    
+    }
+    
 
 function onDocumentReady(){
-    let url = 'http://127.0.0.1:5000/api/v1/questions'
-    fetch(url, {
-        mode: "cors",
-    })
-    .then(res => res.json())
-    // .then(json => console.log(json));
-    .then(json => {
-        console.log(  json.questions[0]);
-        let Qtns = document.getElementById('Qtns');
-            Qtn = createNode('p');
-            console.log(Qtns, Qtn)
+    console.log(document.title=="StackOverflow-lite-index");
+    if (document.title=="StackOverflow-lite-index" || document.title=="StackOverflow-lite-question"){
+        let url = 'http://127.0.0.1:5000/api/v1/questions'
+        fetch(url, {
+            mode: "cors",
+        })
+        .then(res => res.json())
+        // .then(json => console.log(json));
+        .then(json => {
+            console.log(  json.questions[0]);
+            let Qtns = document.getElementById('Qtns');
+                Qtn = createNode('p');
+                console.log(Qtns, Qtn)
 
-        // if (Array.from(json).length !== 0) {
-            
-        //     Qtn.innerHTML = Array.from(json).toString()
-        // }
-        if (Array.from(json).length == 0 && "message" in json){
-            console.log(json.message);
-            Qtn.innerHTML = Array.from(json).toString()
-        }
-        if (Array.from(json).length == 0 && "questions" in json){
-//             console.log(json.questions);
-            console.log(Array.from(json.questions));
-            let ids = [];
-       
-            for (let question of json.questions){
-                ids.push(question.questionId);
-                let a = createNode('a');
-
-                console.log(question);
-                console.log(ids)
-                let li = createNode('li'),
-                    span = createNode('span'),
-                    span2 = createNode('span'),
-                    span3 = createNode('span');
-       
-                span.innerHTML = ` <strong>Author:</strong> ${question.author}`;
-                span2.innerHTML = `${question.body} <br>`;
-                span3.innerHTML = ` <strong>Topic:</strong> ${question.topic} <br>`;
-//                 Qtn.innerHTML = li
-                append(li, span3);
-                append(li, span2);
-                append(li, span);
-                append(Qtn, li);
-                append(Qtns, Qtn);
-
-        
-
+            if (Array.from(json).length == 0 && "message" in json){
+                console.log(json.message);
+                Qtn.innerHTML = Array.from(json).toString()
             }
-        }
-    })
-    .catch(error => console.error(error));
+            if (Array.from(json).length == 0 && "questions" in json){
+    //             console.log(json.questions);
+                console.log(Array.from(json.questions));
+                let ids = [];
+           
+                for (let question of json.questions){
+                    ids.push(question.questionId);
+                    let a = createNode('a');
+                    a.setAttribute('href', 'question.html')
+                    
+                    a.onclick = fetchAnswers(question.questionId);
+                    
+    
+                    console.log(question);
+                    console.log(ids)
+                    let li = createNode('li'),
+                        span = createNode('span'),
+                        span2 = createNode('span'),
+                        span3 = createNode('span');
+           
+                    span.innerHTML = ` <strong>Author:</strong> ${question.author}`;
+                    span2.innerHTML = `${question.body} <br>`;
+                    span3.innerHTML = ` <strong>Topic:</strong> ${question.topic} <br>`;
+    //                 Qtn.innerHTML = li
+                    append(li, span3);
+                    append(li, span2);
+                    append(li, span);
+                    append(Qtn, li);
+                    append(a, Qtn);
+                    append(Qtns, a);
+                }
+            }
+        
+        })
+
+        .catch(error => console.error(error));
+    }
 }
-
-
-
-
-
-// let data = {
-//     "body": "blah blah blah kwemda"
-// }
-
-// if(Array.from(data)) console.log(Array.from(data).length==0);
+    
